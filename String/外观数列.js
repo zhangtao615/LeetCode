a04810819f5907ec2f57db5a4a7e6ff74c9a1c26
zhangtao615 <295564283@qq.com>
2020-08-05 22:23:28 +0800

    countAndSay

---
+++ String/外观数列.js
@@ -0,0 +1,7 @@
+var countAndSay = function(n) {
+    let prev = '1'
+    for(let i = 1; i < n; i++){
+        prev = prev.replace(/(\d)\1*/g, item =>`${item.length}${item[0]}`)
+    }
+    return prev
+}
